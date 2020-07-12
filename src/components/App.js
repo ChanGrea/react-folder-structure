0fb9d9953cdfdf998b94ea22e344f06cdf13da69
ChanGrea <k39335@naver.com>
2020-07-12 11:24:11 +0900

    :hammer: modified: atomic design

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from '../assets/images/logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-function App() {
+import { HomePage, SamplePage, NotFoundPage } from 'components'
+import { GoogleTagManager } from 'containers'
+
+// https://github.com/diegohaz/arc/wiki/Styling
+import theme from './themes/default'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <GoogleTagManager />
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/sample-page" component={SamplePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </ThemeProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './utils/serviceWorker';
+// https://github.com/diegohaz/arc/wiki/Example-app
+import 'react-hot-loader/patch'
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { basename } from 'config'
+import configureStore from 'store/configure'
+import api from 'services/api'
+import App from 'components/App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = configureStore({}, { api: api.create() })
+
+const renderApp = () => (
+  <Provider store={store}>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
+
+const root = document.getElementById('app')
+render(renderApp(), root)
+
+if (module.hot) {
+  module.hot.accept('components/App', () => {
+    require('components/App')
+    render(renderApp(), root)
+  })
+}
